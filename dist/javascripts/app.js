79f723ced5bcf3adfe2ffcf46265b2792ba0dad4
Anna Shelestiuk <anna.shelestyuk@gmail.com>
2020-04-09 19:13:30 +0200

    Cleaup for v2

---
+++ dist/javascripts/app.js
@@ -0,0 +1,6 @@
+jQuery(function ($) {
+  'use strict';
+
+});
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZXNDb250ZW50IjpbImpRdWVyeShmdW5jdGlvbiAoJCkge1xuICAndXNlIHN0cmljdCc7XG5cbn0pO1xuIl19
