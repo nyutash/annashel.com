5a94bec69699c51a2d12b04f12408e96b2aa605a
Anna Shelestiuk <anna.shelestyuk@gmail.com>
2020-04-20 08:22:31 +0200

    Markup and styling

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,4 +1,10 @@
 jQuery(function ($) {
   'use strict';
+  $('.modal-nav-open').click(function() {
+    $('#myNav').height('100%');
+  });
 
+  $('.modal-nav-close').click(function() {
+    $('#myNav').height('0%');
+  });
 });
